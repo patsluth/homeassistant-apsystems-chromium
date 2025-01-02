e54562bdadb5dce1427a5c5b4e9b2e4972ad122e
Patrick Sluth <pat.sluth@gmail.com>
2025-01-02 12:46:50 -0700

    Initial Commit

---
+++ apsystems-api/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for APSystems API integration."""
---
+++ apsystems-api/tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for APSystems API tests."""
+from custom_components.apsystems_api.const import (
+    CONF_PASSWORD,
+)
+from custom_components.apsystems_api.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
